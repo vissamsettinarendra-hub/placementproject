0fff0d37cc95da5f0d1540a4512cd56c2e426618
vissamsettinarendra-hub <vissamsettinarendra@gmail.com>
2026-07-21 22:56:59 +0530

    initial working

--- placement-management-system/src/api/api.js
+++ placement-management-system/src/api/api.js
@@ -3,10 +3,50 @@
 //frontend --->baseURL/students/:id 
 
 import axios from "axios";
-import React from "react";
+
 const api = axios.create({
-    baseURL:"http://localhost:8000"
+    baseURL: "http://localhost:8000/api",
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
+
+// Request Interceptor
+api.interceptors.request.use(
+    (config) => {
+
+        const token = localStorage.getItem("token");
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+// Response Interceptor
+api.interceptors.response.use(
+    (response) => response,
+
+    (error) => {
+
+        if (error.response?.status === 401) {
+
+            localStorage.removeItem("token");
+            localStorage.removeItem("isLoggedIn");
+
+            window.location.href = "/Login";
+        }
+
+        return Promise.reject(error);
+    }
+);
+
 export default api;
 
 //localhost:5000/students/:id 
