4b8c448418981d362ca93e8874cbb5cbd1078408
vissamsettinarendra-hub <vissamsettinarendra@gmail.com>
2026-07-21 23:00:34 +0530

    initial working

--- Placement-Management-Backend/server.js
+++ Placement-Management-Backend/server.js
@@ -1,22 +1,27 @@
-import express from "express"
-import studentRoutes from "./routes/studentRoutes.js";
-import companyRoutes from "./routes/companyRoutes.js"
-import connectDB from "./config/db.js";
+import express from "express";
 import cors from "cors";
-const app = express();
-//Middleware :converts json-object
-app.use(express.json());
-app.use(cors());//allows all origin form devlopment
 
-//call connection DB
+import connectDB from "./config/db.js";
+
+import studentRoutes from "./routes/studentRoutes.js";
+import companyRoutes from "./routes/companyRoutes.js";
+import dashboardRoutes from "./routes/dashboardRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+
+const app = express();
+
 connectDB();
 
-//student routes
-app.use("/students",studentRoutes)
+app.use(cors());
+app.use(express.json());
 
-//company routes
-app.use("/company",companyRoutes)
+// API Routes
+app.use("/api/students", studentRoutes);
+app.use("/api/companies", companyRoutes);
+app.use("/api/dashboard", dashboardRoutes);
+app.use("/api/admin", adminRoutes);
+const PORT = process.env.PORT || 8000;
 
-app.listen(8000,()=>{
-    console.log("server is started at 8000");
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
