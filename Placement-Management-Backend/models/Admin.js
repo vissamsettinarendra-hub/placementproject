28361b4524bbb48002eba16be11a40b9f8f2cc65
vissamsettinarendra-hub <vissamsettinarendra@gmail.com>
2026-07-21 19:33:37 +0530

    initial working

---
+++ Placement-Management-Backend/models/Admin.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const adminSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+        },
+
+        password: {
+            type: String,
+            required: true,
+            minlength: 6,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Admin = mongoose.model("Admin", adminSchema);
+
+export default Admin;
