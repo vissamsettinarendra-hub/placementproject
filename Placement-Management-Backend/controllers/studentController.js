01d555ff498664f175746fa15506375504f55f20
vissamsettinarendra-hub <vissamsettinarendra@gmail.com>
2026-07-21 23:04:29 +0530

    initial working

--- Placement-Management-Backend/controllers/studentController.js
+++ Placement-Management-Backend/controllers/studentController.js
@@ -13,7 +13,7 @@ export const getStudents = async (req, res) => {
         const page = Number(req.query.page) || 1;
         const limit = Number(req.query.limit) || 5;
 
-        const skip = (page - 1) * limit;
+        const skip = (page - 1) * limit ;
 
         const totalStudents = await Student.countDocuments();
 
