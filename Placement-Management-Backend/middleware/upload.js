d704d66b52d0211c0b1c0d427c5da6653f621d47
vissamsettinarendra-hub <vissamsettinarendra@gmail.com>
2026-07-22 22:12:14 +0530

    initial working

---
+++ Placement-Management-Backend/middleware/upload.js
@@ -0,0 +1,35 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads/");
+    },
+
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + "-" + file.originalname);
+    },
+});
+
+const fileFilter = (req, file, cb) => {
+
+    if (
+        file.mimetype === "image/jpeg" ||
+        file.mimetype === "image/jpg" ||
+        file.mimetype === "image/png"
+    ) {
+        cb(null, true);
+    } else {
+        cb(new Error("Only JPG, JPEG and PNG images are allowed"), false);
+    }
+
+};
+
+const upload = multer({
+    storage,
+    fileFilter,
+    limits: {
+        fileSize: 5 * 1024 * 1024, // 5 MB
+    },
+});
+
+export default upload;
