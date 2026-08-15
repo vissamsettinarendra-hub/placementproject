de6ad8e31a6d37cad82a1a38e01578f0409f7e27
vissamsettinarendra-hub <vissamsettinarendra@gmail.com>
2026-08-15 23:13:06 +0530

    ready for deployment...

--- Placement-Management-Backend/controllers/authController.js
+++ Placement-Management-Backend/controllers/authController.js
@@ -2,6 +2,8 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 import User from "../models/User.js";
+
+
 // ===========================
 // Register User
 // ===========================
